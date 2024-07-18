8806d9c611f37aa6957210fd1d960b46e91c9625
Aakash <iakashman8653@gmail.com>
2024-07-18 16:23:14 +0530

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { SignupComponent } from './auth/components/signup/signup.component';
 
 const routes: Routes = [
   { path: "login", component: LoginComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  // { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: "signup", component: SignupComponent },
   { path: "admin", loadChildren: () => import("./modules/admin/admin.module").then(e => e.AdminModule) },
   { path: "employee", loadChildren: () => import("./modules/employee/employee.module").then(e => e.EmployeeModule) }
